d934621d55915c9287d5aa2aa5506b0c7e0102a9
reza <ahmad.reza.musthafa2@gmail.com>
2025-02-22 06:44:41 +0700

    adjust order repository

--- internal/adapters/repositories/order_repository.go
+++ internal/adapters/repositories/order_repository.go
@@ -1,8 +1,12 @@
 package repositories
 
 import (
+	"context"
+	"github.com/ahmadrezamusthafa/ecommerce/internal/core/domain/entity"
 	"github.com/ahmadrezamusthafa/ecommerce/internal/core/ports"
+	"github.com/ahmadrezamusthafa/ecommerce/internal/shared/constants"
 	"gorm.io/gorm"
+	"time"
 )
 
 type orderRepository struct {
@@ -13,4 +17,33 @@ func NewOrderRepository(db *gorm.DB) ports.IOrderRepository {
 	return &orderRepository{
 		db: db,
 	}
+}
+func (r *orderRepository) CreateOrder(ctx context.Context, order entity.Order) (entity.Order, error) {
+	ctx, cancel := context.WithTimeout(ctx, constants.DefaultHTTPReadTimeout)
+	defer cancel()
+
+	order.OrderDate = time.Now()
+	if err := r.db.WithContext(ctx).Create(&order).Error; err != nil {
+		return entity.Order{}, err
+	}
+	return order, nil
+}
+
+func (r *orderRepository) GetTopCustomers(ctx context.Context, limit int) ([]entity.Order, error) {
+	ctx, cancel := context.WithTimeout(ctx, constants.DefaultHTTPReadTimeout)
+	defer cancel()
+
+	var orders []entity.Order
+	err := r.db.WithContext(ctx).
+		Select("customer_id, SUM(amount) AS total_amount").
+		Where("order_date >= NOW() - INTERVAL '1 MONTH'").
+		Group("customer_id").
+		Order("total_amount DESC").
+		Limit(limit).
+		Find(&orders).Error
+
+	if err != nil {
+		return nil, err
+	}
+	return orders, nil
 }
--- internal/core/domain/entity/order.go
+++ internal/core/domain/entity/order.go
@@ -3,9 +3,9 @@ package entity
 import "time"
 
 type Order struct {
-	ID         int        `db:"id" json:"id"`
-	CustomerID int        `db:"customer_id" json:"customer_id"`
-	ProductID  int        `db:"product_id" json:"product_id"`
-	Amount     float64    `db:"amount" json:"amount"`
-	OrderDate  *time.Time `db:"order_date" json:"order_date"`
+	ID         int       `db:"id" json:"id"`
+	CustomerID int       `db:"customer_id" json:"customer_id"`
+	ProductID  int       `db:"product_id" json:"product_id"`
+	Amount     float64   `db:"amount" json:"amount"`
+	OrderDate  time.Time `db:"order_date" json:"order_date"`
 }
